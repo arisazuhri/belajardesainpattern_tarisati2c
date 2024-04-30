39d8578749267b3bf9ff03b4cdf254f7cdca0547
arisazuhri <146687915+arisazuhri@users.noreply.github.com>
2024-04-30 10:23:49 +0700

    Add files via upload

---
+++ SimUDuck/src/tahap3/Main.java
@@ -0,0 +1,14 @@
+package tahap3;
+
+public class Main {
+    public static void main(String[] args){
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
+        Duck modelduck = new ModelDuck();
+        modelduck.performFly(); // Akan memanggil constructor modelduck(FlyNoWay())
+        modelduck.setFlyBehavior(new FlyWithRocketPower());
+        modelduck.performFly();
+    }
+}
